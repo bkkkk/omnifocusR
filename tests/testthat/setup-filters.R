73c43a3591fc9e2461fea200d00e4452ba02f908
Jacobo Blanco <jayb88@gmail.com>
2019-01-07 01:25:05 +0900

    Formatting of test xml

--- tests/testthat/setup-filters.R
+++ tests/testthat/setup-filters.R
@@ -1,73 +1,73 @@
-test_document_for_filters <-
-  read_xml('<?xml version="1.0" encoding="UTF-8"?>
-           <omnifocus app-id="com.omnigroup.OmniFocus3" app-version="120.11.0.323402" machine-model="MacBookPro14,1" os-name="Mac OS X" os-version="10.14" xmlns="http://www.omnigroup.com/namespace/OmniFocus/v2">
-           <task id="gg0bK99uSq5">
-           <project>
-           <folder idref="fh4AfJuvkWq"/>
-           <singleton>false</singleton>
-           <last-review>2018-11-05T08:30:38.213Z</last-review>
-           <next-review/>
-           <review-interval>@1w</review-interval>
-           <status>done</status>
-           </project>
-           <inbox>false</inbox>
-           <task/>
-           <added>2018-10-18T00:56:18.342Z</added>
-           <name>Have built JIRA system for merchant on-boarding</name>
-           <note/>
-           <rank>0</rank>
-           <context/>
-           <start>2018-11-05T23:00:00.000Z</start>
-           <due/>
-           <completed>2018-11-09T09:15:14.937Z</completed>
-           <estimated-minutes/>
-           <order>sequential</order>
-           <flagged>true</flagged>
-           <completed-by-children>false</completed-by-children>
-           <repetition-rule/>
-           <repetition-method/>
-           <next-clone-identifier>0</next-clone-identifier>
-           <due-date-alarm-policy/>
-           <defer-date-alarm-policy/>
-           <latest-time-to-start-alarm-policy/>
-           <modified>2018-11-09T09:15:15.077Z</modified>
-           </task>
-           <task id="kWht8GKY4h8">
-           <project/>
-           <inbox>false</inbox>
-           <task idref="gg0bK99uSq5"/>
-           <added>2018-10-18T01:37:13.089Z</added>
-           <name>Look into creating re-ocurring sub tasks on JIRA</name>
-           <note/>
-           <rank>0</rank>
-           <context idref="osOs7sKsHJ5"/>
-           <start/>
-           <due/>
-           <completed>2018-10-29T08:54:18.906Z</completed>
-           <estimated-minutes/>
-           <order>sequential</order>
-           <flagged>false</flagged>
-           <completed-by-children>false</completed-by-children>
-           <repetition-rule/>
-           <repetition-method/>
-           <next-clone-identifier>0</next-clone-identifier>
-           <due-date-alarm-policy/>
-           <defer-date-alarm-policy/>
-           <latest-time-to-start-alarm-policy/>
-           <modified>2018-10-29T08:54:18.917Z</modified>
-           </task>
-           <context id="nIr8xE7NZof">
-           <added order="12">2015-06-02T08:27:56.678Z</added>
-           <modified>2016-11-19T13:02:08.968Z</modified>
-           <name>Waiting</name>
-           <rank>-1573330046</rank>
-           <prohibits-next-action>true</prohibits-next-action>
-           </context>
-           <task-to-tag id="fhPlxmBdyCS.9.nFSbMtPEGrh">
-		       <added order="21">2019-01-02T02:10:31.370Z</added>
-		       <task idref="fhPlxmBdyCS.9"/>
-		       <context idref="nFSbMtPEGrh"/>
-		       <rank-in-task>0001</rank-in-task>
-		       <rank-in-tag/>
-	         </task-to-tag>
-           </omnifocus>')
+test_document_for_filters <- read_xml(
+'<?xml version="1.0" encoding="UTF-8"?>
+<omnifocus app-id="com.omnigroup.OmniFocus3" app-version="120.11.0.323402" machine-model="MacBookPro14,1" os-name="Mac OS X" os-version="10.14" xmlns="http://www.omnigroup.com/namespace/OmniFocus/v2">
+  <task id="gg0bK99uSq5">
+    <project>
+      <folder idref="fh4AfJuvkWq"/>
+      <singleton>false</singleton>
+      <last-review>2018-11-05T08:30:38.213Z</last-review>
+      <next-review/>
+      <review-interval>@1w</review-interval>
+      <status>done</status>
+    </project>
+    <inbox>false</inbox>
+    <task/>
+    <added>2018-10-18T00:56:18.342Z</added>
+    <name>Have built JIRA system for merchant on-boarding</name>
+    <note/>
+    <rank>0</rank>
+    <context/>
+    <start>2018-11-05T23:00:00.000Z</start>
+    <due/>
+    <completed>2018-11-09T09:15:14.937Z</completed>
+    <estimated-minutes/>
+    <order>sequential</order>
+    <flagged>true</flagged>
+    <completed-by-children>false</completed-by-children>
+    <repetition-rule/>
+    <repetition-method/>
+    <next-clone-identifier>0</next-clone-identifier>
+    <due-date-alarm-policy/>
+    <defer-date-alarm-policy/>
+    <latest-time-to-start-alarm-policy/>
+    <modified>2018-11-09T09:15:15.077Z</modified>
+  </task>
+  <task id="kWht8GKY4h8">
+    <project/>
+    <inbox>false</inbox>
+    <task idref="gg0bK99uSq5"/>
+    <added>2018-10-18T01:37:13.089Z</added>
+    <name>Look into creating re-ocurring sub tasks on JIRA</name>
+    <note/>
+    <rank>0</rank>
+    <context idref="osOs7sKsHJ5"/>
+    <start/>
+    <due/>
+    <completed>2018-10-29T08:54:18.906Z</completed>
+    <estimated-minutes/>
+    <order>sequential</order>
+    <flagged>false</flagged>
+    <completed-by-children>false</completed-by-children>
+    <repetition-rule/>
+    <repetition-method/>
+    <next-clone-identifier>0</next-clone-identifier>
+    <due-date-alarm-policy/>
+    <defer-date-alarm-policy/>
+    <latest-time-to-start-alarm-policy/>
+    <modified>2018-10-29T08:54:18.917Z</modified>
+  </task>
+  <context id="nIr8xE7NZof">
+    <added order="12">2015-06-02T08:27:56.678Z</added>
+    <modified>2016-11-19T13:02:08.968Z</modified>
+    <name>Waiting</name>
+    <rank>-1573330046</rank>
+    <prohibits-next-action>true</prohibits-next-action>
+  </context>
+  <task-to-tag id="fhPlxmBdyCS.9.nFSbMtPEGrh">
+    <added order="21">2019-01-02T02:10:31.370Z</added>
+    <task idref="fhPlxmBdyCS.9"/>
+    <context idref="nFSbMtPEGrh"/>
+    <rank-in-task>0001</rank-in-task>
+    <rank-in-tag/>
+  </task-to-tag>
+</omnifocus>')
