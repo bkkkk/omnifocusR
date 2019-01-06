66f02d79f10fedc3a8f043cfcb9b6a42e8078b3d
Jacobo Blanco <jayb88@gmail.com>
2019-01-06 17:19:40 +0900

    Parse tasks

---
+++ tests/testthat/setup-parse-tasks.R
@@ -0,0 +1,27 @@
+test_document_with_single_task <- read_xml('<?xml version="1.0" encoding="UTF-8"?>
+           <omnifocus app-id="com.omnigroup.OmniFocus3" app-version="120.11.0.323402" machine-model="MacBookPro14,1" os-name="Mac OS X" os-version="10.14" xmlns="http://www.omnigroup.com/namespace/OmniFocus/v2">
+           <task id="kWht8GKY4h8">
+           <project/>
+           <inbox>false</inbox>
+           <task idref="gg0bK99uSq5"/>
+           <added>2018-10-18T01:37:13.089Z</added>
+           <name>Look into creating re-ocurring sub tasks on JIRA</name>
+           <note/>
+           <rank>0</rank>
+           <context idref="osOs7sKsHJ5"/>
+           <start/>
+           <due/>
+           <completed>2018-10-29T08:54:18.906Z</completed>
+           <estimated-minutes/>
+           <order>sequential</order>
+           <flagged>false</flagged>
+           <completed-by-children>false</completed-by-children>
+           <repetition-rule/>
+           <repetition-method/>
+           <next-clone-identifier>0</next-clone-identifier>
+           <due-date-alarm-policy/>
+           <defer-date-alarm-policy/>
+           <latest-time-to-start-alarm-policy/>
+           <modified>2018-10-29T08:54:18.917Z</modified>
+           </task>
+           </omnifocus>')
